6098e1ef0b5b2509cedc3e54d707f3c17f8b6ff6
jingyadong <37835824+jingyadong@users.noreply.github.com>
2021-04-13 08:50:12 +0800

    Update merkle-tree.py

--- merkle-tree.py
+++ merkle-tree.py
@@ -1,40 +1,39 @@
 import hashlib
 
-def is_power_of_two(n):
-    return n != 0 and ((n & (n-1)) == 0)
-
 def hash_data(data, hash_function = 'sha256'):
-    '''hash function'''
+    "hash function"
     hash_function = getattr(hashlib, hash_function)
     data = data.encode('utf-8')
     return hash_function(data).hexdigest()
 
 def concat_and_hash_list(lst, hash_function = 'sha256'):
-    #quickly concatenate pairs of values and hash them.
-    '''
-    concat_and_hash_list(['a','b','c'])=hash( hash(ab)+c )
-    'c9e620703fe0f117429c21ec19323aff8673ac05ae8ddd7bcc36599aabc34c51'
-    '''
-    assert len(lst) >= 2, "no transactions to be hashed"
-    while len(lst)>1:
-        if len(lst)%2 ==0:
+    lst1 = []
+    for i in lst:
+        lst1.append(hash_data(i))
+    # print(lst1)
+
+    assert len(lst1)>2, "no tracnsactions to be hashed"
+    n = 0 #merkle树高度
+    while len(lst1) >1:
+        n += 1
+        if len(lst1)%2 == 0:
             v = []
-            while len(lst) > 1:
-                a = lst.pop(0)
-                b = lst.pop(s0)
-                v.append(hash_data(a+b,hash_function))
-                # v.append(a+b)
-            lst = v
+            while len(lst1) >1 :
+                a = lst1.pop(0)
+                b = lst1.pop(0)
+                v.append(hash_data(a+b, hash_function))
+            lst1 = v
         else:
             v = []
-            l = lst.pop(-1)
-            while len(lst) >1:
-                a = lst.pop(0)
-                b = lst.pop(0)
-                v.append(hash_data(a+b,hash_function))
-                # v.append(a+b)
+            l = lst1.pop(-1)
+            while len(lst1) >1 :
+                a = lst1.pop(0)
+                b = lst1.pop(0)
+                v.append(hash_data(a+b, hash_function))
             v.append(l)
-            lst = v
-    return lst
+            lst1 = v
+    return lst1, n+1
 
 
+l = ['a', 'b', 'c',"d"]
+print(concat_and_hash_list(l))
